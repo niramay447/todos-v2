2d25246904061db15d944e1f1356a5206fc89c16
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-06-21 18:01:55 +0530

    update home page after deletion

--- app.js
+++ app.js
@@ -67,8 +67,15 @@ app.post("/", function(req, res){
 });
 
 app.post("/delete", function(req,res){
-  console.log(req.body.checkbox);
-})
+  const checkedItemId = req.body.checkbox;
+
+  Item.findByIdAndRemove(checkedItemId, function(err){
+    if(!err){
+      console.log("Successfully deleted checked item");
+      res.redirect("/");
+    }
+  });
+});
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
